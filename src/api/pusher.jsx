3b9590ec883508edcf719cb49aec997ca9be9e2d
Abdelhamied <Abdelhamied404@gmail.com>
2019-07-06 19:00:55 +0200

    stop pusher from logging to console

--- src/api/pusher.jsx
+++ src/api/pusher.jsx
@@ -3,7 +3,7 @@ import Pusher from "pusher-js";
 const IS_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 const key = IS_DEV ? process.env.REACT_APP_DEV_PUSHER_KEY : process.env.REACT_APP_PROD_PUSHER_KEY;
 
-Pusher.logToConsole = true;
+Pusher.logToConsole = false;
 
 export const pusher = new Pusher(key, {
   cluster: "mt1",
